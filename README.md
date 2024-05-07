3b9a7e47cafdcd9621e0528c7282ec56dbf9cd61
TKStrijker <timokstrijker@gmail.com>
2024-05-07 19:45:52 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,5 @@
+## setup
+
+## Usage
+
+Simply run `php artisan serve --host=8001`, the API should now be up and running.
---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,4 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+    The Api is up and running.
+</html>
